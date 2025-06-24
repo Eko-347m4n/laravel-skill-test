f6447c279d8ea72ed4c669ff0e55d9a92a222c00
Eko-347m4n <347m4n2024@gmail.com>
2025-06-24 21:43:48 +0700

    chore: redirect root route to login page

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('welcome');
+Route::redirect('/', '/login');
+
+Route::get('/home', function () {
+    return redirect()->route('dashboard');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('dashboard', function () {
-        return Inertia::render('dashboard');
-    })->name('dashboard');
-});
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
